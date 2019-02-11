f708b4671675d592789788ceae4293c6fd5e232f
barathwaj <barathcjb@gmail.com>
2019-02-11 21:20:35 +0530

    added db functionality

--- auth-db-handler/handler.py
+++ auth-db-handler/handler.py
@@ -9,26 +9,43 @@ __email__ = 'barathcjb@gmail.com'
 
 import os
 import sqlite3 as sqlite
+import crypt
+import hashlib
 
 
-class CreateDatabase(sqlite.Connection):
-    def __init__(self):
-        sqlite.Connection.__init__(self, **args)
-        self._cur = self.cursor()
-        self._table = []
-        self._attributes = []
+class CreateDatabase(object):
+    def __init__(self, database, timeout=20, password=None):
+        self.connection = sqlite.connect(database=database, timeout=timeout)
+        self.connection.set_authorizer
+        self.cur = self.connection.cursor()
+        self.table = []
+        self.attributes = []
+        if password != None:
+            self.password = hashlib.sha3_256(password).hexdigest()
+        self.cur.execute("PRAGMA key=%s"%(self.password))
 
     def createTable(table: str, attribute: tuple):
-        self._attributes.append(attribute)
-        self._table.append(table)
-        self._cur.execute("create table %s %s" % (table, attribute))
+        self.attributes.append(attribute)
+        self.table.append(table)
+        self.cur.execute("create table %s %s" % (table, attribute))
 
     def viewDatabase():
-        dbviews = []
-        for (_table, _attri) in zip(self._table, self._attributes):
-            dbviews.append((_table, _attri))
-        dbviews.clear()
-        return dbviews
+        db_views = []
+        for (table, _attri) in zip(self.table, self.attributes):
+            db_views.append((table, _attri))
+        return db_views
 
     def viewTable(table: str, conditions: str):
-        self._cur.execute("select * from %s %s" % (table, conditions))
+        table_views = []
+        rows = self.cur.execute("select * from %s %s" % (table, conditions))
+        for _items in rows:
+            table_views.append(_items)
+        return table_views
+
+    def viewRow(table: str, attribute: tuple, conditions: str):
+        select_rows = []
+        rows = self.cur.execute('select %s from %s where %s' %
+                                (table, attribute, conditions))
+        for _items in rows:
+            select_rows.append(_items)
+        return select_rows
