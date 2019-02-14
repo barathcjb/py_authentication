216cb6024cccffc9e4d2a1ae06e5516fde316e9c
barathwaj <barathcjb@gmail.com>
2019-02-14 20:53:06 +0530

    updated init.py and readme

--- auth-db-handler/__init__.py
+++ auth-db-handler/__init__.py
@@ -3,9 +3,9 @@
 # MIT License
 # Copyright (c) 2019 Barathwaj
 
-__project__ = "authentication database handler"
+__project__ = "python user authentication library"
 __version__ = '0.0.1'
 __author__ = 'Barathwaj C'
 __email__ = 'barathcjb@gmail.com'
-__description__ = "minimal python library to store, manipulate and \
-   retrieve passwords into new/existing sqlite3 database"
+__description__ = "pure pythonic library to store, manipulate and \
+   retrieve usernames and passwords into new/existing sophisticated datafile"
